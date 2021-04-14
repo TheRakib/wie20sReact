a205f09363b8c04bfff44205d59c50727d5295eb
RakibTest <rakibmedieinstitutet@gmail.com>
2021-04-14 12:00:26 +0200

    state

--- wie20s/src/components/card.js
+++ wie20s/src/components/card.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+// state:  hur vi kan passsera data inuti componentet
 
+// event : onClick , onChange, onSubmit 
 
+// hur vi använder state ? 
+  //Steg 1: import { useState} from "react"
+  //Steg 2: Ange namn och sätt ett default value const [counter, setCounter] = useState(0)
+             // när du skriver ut value från state :counter
+             // för att kunna sätta värdet : setCounter
+
+// steg 3: sätter vi value till state : setCounter , onclick
+
+// Steg 4: renderar/skriver man ut state: counter
+
+//obs: När man ändrar state rerenderas hela component.
 // Varifrån kom props ?
 // varför lägger vi så {props.name}  {  }? 
-
 // När vi lägger javascript i jsx lägger vi { }
-
 // Varför använder vi props? 
 function Card(props) {
+    const [name, setName] = useState(" Hello ")
+     //  ()=> { } arrow function 
     return(
         <>
-         <div>Name: {props.name} </div>
+         <div> <button onClick={ ()=> { setName("hej") } }>+</button>   </div>
 
+         
+         <button onClick={ ()=>{ setName("Hallå")}  }   > - </button>
+         <div>{name}   </div> 
+        
         <div> Price: {props.price} </div>
        
         </>
