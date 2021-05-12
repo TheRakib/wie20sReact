087146ae07d33845832629b3d5ea43f80f881b6c
RakibTest <rakibmedieinstitutet@gmail.com>
2021-05-12 10:18:14 +0200

    pagination del 2

--- bookingapp2021/src/components/CardList.js
+++ bookingapp2021/src/components/CardList.js
@@ -8,12 +8,15 @@ function CardList() {
 
     // useState for products 
     const [products, setProducts] = useState([]);
-    const [loadPage, setLoadPage]= useState(2)
+
+    const [loadPage, setLoadPage]= useState(2);
+
+    //const [responseData , setResponseData]= useState([])
 
 
      // useEffect för att kunna hämta data från database 
     useEffect(()=>{ 
-        console.log("from useEffect" , loadPage)
+   //console.log("from useEffect" , loadPage)
         const fecthProducts= async()=>{
         // query params/query string? 
         // vi vill inte läsa allt från databasen på en gång
@@ -21,17 +24,16 @@ function CardList() {
            const response =   await axios.get(`http://localhost:1337/products?_limit=${loadPage}`)
            console.log(response)
            setProducts(response.data)
+
         }
         fecthProducts()
-
     }, [loadPage])
-
-   
      // skriva load more function 
      function loadMore() {
-
         // kolla hur många produkter finns i database 
-        
+
+        console.log("length of product array" , products.length)
+
          let dynamicPage = loadPage + 2 ;
          console.log("load more", loadPage)
          setLoadPage(dynamicPage)
@@ -39,19 +41,25 @@ function CardList() {
      }
 
 
+
     // skriva visa färre function
+    function showLess(){
 
-
+    }
     return (
         <div>
-            
              {products.map((product)=>{
                  return (
                      <Card key={product.id} image={product.img}  productName={product.name}  price={product.price} description= {product.description} />
                  )
              }) }
-               
-               <button onClick={loadMore}>Load more</button>
+
+ 
+ 
+ { (products.length >loadPage || products.length === loadPage)   ? 
+               <button onClick={loadMore}>Load more</button> 
+               :
+               <button onClick={showLess}>Show less</button> }
         </div>
     )
 }
@@ -61,4 +69,4 @@ export default CardList
 // rfce
 // 11.00
 
-
+// kl. 10.00
